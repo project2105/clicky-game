f4459a2843b2a39a4b58accb620a473f3abf5dd8
pfjensen <slush34@att.net>
2018-10-05 14:57:20 -0500

    Updates

---
+++ precache-manifest.9e025b101e4da2b2b1767af770748881.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "6e35114a22d36c749ae73114e7ac87bd",
+    "url": "/clicky-game/static/media/derangedEmoji.6e35114a.jpg"
+  },
+  {
+    "revision": "f61f67fea17a5aab52f3",
+    "url": "/clicky-game/static/css/main.bf41eb28.chunk.css"
+  },
+  {
+    "revision": "169350929a3e9631ff63",
+    "url": "/clicky-game/static/js/1.16935092.chunk.js"
+  },
+  {
+    "revision": "6acb6692e4ee25941da6",
+    "url": "/clicky-game/static/js/runtime~main.6acb6692.js"
+  },
+  {
+    "revision": "353d4245ac870d346cab5402a1ebd4f5",
+    "url": "/clicky-game/static/media/angryEmoji.353d4245.jpg"
+  },
+  {
+    "revision": "b9d716a7f94b7ea9a817796b02d658c0",
+    "url": "/clicky-game/static/media/awedEmoji.b9d716a7.jpg"
+  },
+  {
+    "revision": "3e2439e20e1c9db522d6bfb6ebc47ab0",
+    "url": "/clicky-game/static/media/coolEmoji.3e2439e2.jpg"
+  },
+  {
+    "revision": "113470addd6fbdd36efe0c02a3a44d19",
+    "url": "/clicky-game/static/media/cryingEmoji.113470ad.jpg"
+  },
+  {
+    "revision": "f61f67fea17a5aab52f3",
+    "url": "/clicky-game/static/js/main.f61f67fe.chunk.js"
+  },
+  {
+    "revision": "48600f6c4d6c4cae32f9c4540ccf46a6",
+    "url": "/clicky-game/static/media/fearfulEmoji.48600f6c.jpg"
+  },
+  {
+    "revision": "19c59be22de2bd0c2a68f0d37b6a2b2e",
+    "url": "/clicky-game/static/media/salivatingEmoji.19c59be2.jpg"
+  },
+  {
+    "revision": "1d08d36340fce00367f12d1d9b657c4f",
+    "url": "/clicky-game/static/media/sinisterEmoji.1d08d363.jpg"
+  },
+  {
+    "revision": "934619e57e52e61e60ba7bd5e082c93d",
+    "url": "/clicky-game/static/media/smilingEmoji.934619e5.jpg"
+  },
+  {
+    "revision": "08a67b7610a365e2f91b992ae60084fd",
+    "url": "/clicky-game/static/media/vulnerableEmoji.08a67b76.jpg"
+  },
+  {
+    "revision": "9497eed95985c71aca5704a625e2b5b2",
+    "url": "/clicky-game/static/media/winkingEmoji.9497eed9.jpg"
+  },
+  {
+    "revision": "4b709a8a7301c1dd1dc714c947b2932b",
+    "url": "/clicky-game/index.html"
+  }
+];
